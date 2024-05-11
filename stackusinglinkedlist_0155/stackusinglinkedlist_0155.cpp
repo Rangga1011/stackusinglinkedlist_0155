dfb36a6ecb0c6f2f37ab16c568187f832230dda5
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-11 11:08:32 +0700

    Menambahkan int main case 1

--- stackusinglinkedlist_0155/stackusinglinkedlist_0155.cpp
+++ stackusinglinkedlist_0155/stackusinglinkedlist_0155.cpp
@@ -62,6 +62,30 @@ public:
     }
 };
 
+int main() {
+    Stack stack;
+
+    int  choice = 0;
+    int value;
+
+    while (choice != 5) {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice != 5) {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);  // Push the entered value onto the stack
+            break;
+        }
+    }
+}
+
 
 
 
