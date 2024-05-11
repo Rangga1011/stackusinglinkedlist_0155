19cac2048f6827792af09e9021e5953a51b8a858
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-11 11:19:50 +0700

    Menambahkan int main case 4

--- stackusinglinkedlist_0155/stackusinglinkedlist_0155.cpp
+++ stackusinglinkedlist_0155/stackusinglinkedlist_0155.cpp
@@ -100,8 +100,19 @@ int main() {
                 cout << "Stack is empty. No top value." << endl;
             }
             break;
+
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
         }
+
+        cout << endl;
     }
+
+    return 0;
 }
 
 
